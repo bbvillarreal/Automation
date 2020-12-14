2f203bb2a3c7a14a3ad9a32d2eab5d17672d9d9b
bbvillarreal <bbvillarreal9@gmail.com>
2020-12-14 14:00:12 -0600

    Changed url to my azure account
    Changed URL

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,10 +19,10 @@ public class BrowserTest {
 		System.out.println("Hello Guys and Gals");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("https://blasbvillarreal.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("BlasVillarrealAcademy.com Learning"));
 		driver.close();
 	
 		
